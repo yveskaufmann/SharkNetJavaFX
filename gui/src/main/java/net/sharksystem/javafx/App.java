6a15f18b1f246919514e3bd57fbe89d9e910029f
Yves Kaufmann <fxdapokalypse@googlemail.com>
2016-05-12 18:53:59 +0200

    Initial Commit

---
+++ gui/src/main/java/net/sharksystem/javafx/App.java
@@ -0,0 +1,17 @@
+package net.sharksystem.javafx;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
