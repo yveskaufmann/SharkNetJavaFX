07c8aed8dcf8a3770cd0e8bd87f1ec942bb7226d
johann44 <johann.w@live.de>
2016-06-05 19:24:36 +0200

    newContactView + ContactNewController created

--- sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/ContactController.java
+++ sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/ContactController.java
@@ -3,6 +3,7 @@ package net.sharksystem.sharknet.javafx.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.event.ActionEvent;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -10,6 +11,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import net.sharksystem.sharknet.api.*;
 import net.sharksystem.sharknet.javafx.App;
+import net.sharksystem.sharknet.javafx.controller.FrontController;
+import net.sharksystem.sharknet.javafx.model.SharkNetModel;
 import net.sharksystem.sharknet.javafx.utils.controller.Controllers;
 import net.sharksystem.sharknet.javafx.utils.controller.annotations.Controller;
 import net.sharksystem.sharknet.javafx.utils.controller.AbstractController;
@@ -24,15 +27,15 @@ public class ContactController extends AbstractController{
 
 	private FrontController appController;
 
-	private ImplSharkNet implSharkNet;
+	private ImplSharkNet sharkNetModel;
 	//private ImplContact testKontakt3 = new ImplContact("Jan", "", "");
 
 	public ContactController() {
 		super(App.class.getResource("views/contactsView.fxml"));
 		this.appController = Controllers.getInstance().get(FrontController.class);
 
-		implSharkNet = new ImplSharkNet();
-		implSharkNet.fillWithDummyData();
+		sharkNetModel = SharkNetModel.getInstance().getSharkNetImpl();
+
 	}
 
 
@@ -53,6 +56,9 @@ public class ContactController extends AbstractController{
 	@FXML
 	private void onNewContactButtonClick() {
 		System.out.println("Neuen Kontakt erstellen");
+
+		ContactNewController c = new ContactNewController();
+
 		//appController.getChildren().clear();
 		//appController.getChildren().add(FXMLLoader.load(getClass().getResource("Content2.fxml"));
 	}
@@ -62,12 +68,6 @@ public class ContactController extends AbstractController{
 		System.out.println("Kontakt löschen:");
 		System.out.println(contactList.getSelectionModel().getSelectedItem());
 		//testKontakt.deleteContact();
-	}
-
-	@FXML
-	private void onContactGroupButtonClick() {
-		System.out.println("Kontakt zu Gruppe:");
-		System.out.println(contactList.getSelectionModel().getSelectedItem());
 	}
 
 	@FXML
@@ -83,22 +83,13 @@ public class ContactController extends AbstractController{
 	}
 
 
-	//
-	Image getImageForContact(String contactName){
-
-		return null;
-	}
-	//
-
 
 	@Override
 	protected void onFxmlLoaded() {
 
 		ObservableList<String> contactsData = FXCollections.observableArrayList();
 		ObservableList<String> blackListData = FXCollections.observableArrayList();
-		ObservableList<String> groupsData = FXCollections.observableArrayList("Gruppe1","Gruppe2", "Gruppe3");
 		FilteredList<String> filteredContacts = new FilteredList<>(contactsData, p -> true);
-		FilteredList<String> filteredGroups = new FilteredList<>(groupsData, p -> true);
 		FilteredList<String> filteredBlocked = new FilteredList<>(blackListData, p -> true);
 
 		//TEST Suchfunktion
@@ -118,22 +109,6 @@ public class ContactController extends AbstractController{
 			});
 		});
 
-		// Gruppen filtern
-		groupsSearchTextfield.textProperty().addListener((observable, oldValue, newValue) -> {
-			filteredGroups.setPredicate(nickname -> {
-				// If filter text is empty, display all entries.
-				if (newValue == null || newValue.isEmpty()) {
-					return true;
-				}
-				// Compare first name and last name of every person with filter text.
-				String lowerCaseFilter = newValue.toLowerCase();
-
-				if (nickname.toLowerCase().contains(lowerCaseFilter)) {
-					return true; // Filter matches first name.
-				}
-				return false; // Does not match.
-			});
-		});
 
 		// Blacklist filtern
 		blacklistSearchTextfield.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -153,15 +128,8 @@ public class ContactController extends AbstractController{
 		});
 
 
-		//Mit Testwerten füllen
-		List<Contact> allContacts = implSharkNet.getContacts();
-		//List<ImplContact> allContacts = new ArrayList<ImplContact>();
+		List<Contact> allContacts = sharkNetModel.getContacts();
 		List<ImplContact> allBlockedContacts = new ArrayList<ImplContact>();
-		//List<ImplGroup> allGroups = new ArrayList<ImplGroup>();
-
-
-		//allBlockedContacts.add(testKontakt3);
-
 
 
 		// Kontaktlisten füllen
@@ -182,11 +150,9 @@ public class ContactController extends AbstractController{
 
 
 		contactList.setItems(filteredContacts);
-		groupList.setItems(filteredGroups);
 		blackList.setItems(filteredBlocked);
 		//contactList.setItems(contactsData);
 		//blackList.setItems(blackListData);
-		//groupList.setItems(groupsData);
 
 		contactList.setCellFactory(listView -> new ListCell<String>() {
 			private ImageView imageView = new ImageView();
@@ -206,27 +172,6 @@ public class ContactController extends AbstractController{
 			}
 		});
 
-
-		// TODO Gruppenbild
-		/*
-		groupList.setCellFactory(listView -> new ListCell<String>() {
-			private ImageView imageView = new ImageView();
-			@Override
-			public void updateItem(String contactName, boolean empty) {
-				super.updateItem(contactName, empty);
-				if (empty) {
-					setText(null);
-					setGraphic(null);
-				} else {
-					//Image image = getImageForContact(contactName);
-					Image profilePicture = new Image(App.class.getResource("images/profile.png").toExternalForm());
-					imageView.setImage(profilePicture);
-					setText(contactName);
-					setGraphic(imageView);
-				}
-			}
-		});
-		*/
 
 		blackList.setCellFactory(listView -> new ListCell<String>() {
 			private ImageView imageView = new ImageView();
