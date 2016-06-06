8ce90ee3ef0a87dacf50a0345ff44214eb882580
Yves Kaufmann <fxdapokalypse@googlemail.com>
2016-06-06 22:32:07 +0200

    Fix fixed path in EmojiController

--- sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/chat/Emoji.java
+++ sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/chat/Emoji.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Benni on 06.06.2016.
@@ -17,16 +18,17 @@ public class Emoji {
 	public static final int rows = 43;
 	private final int width = 64;
 	private final int height = 64;
+	private InputStream in;
 
 
-	public Emoji(String fileName) {
-		this.fileName = fileName;
+	public Emoji(InputStream in) {
+		this.in = in;
 		emojis = new BufferedImage[cols][rows];
 	}
 
 	public void loadEmojis() {
 		try {
-			bufImage = ImageIO.read(new File(fileName));
+			bufImage = ImageIO.read(in);
 			for (int i = 0; i < cols; i++)
 			{
 				for (int j = 0; j < rows; j++)
--- sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/chat/EmojiController.java
+++ sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/chat/EmojiController.java
@@ -16,6 +16,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 
 /**
  * Created by Benni on 06.06.2016.
@@ -34,22 +35,30 @@ public class EmojiController extends AbstractController {
 		Parent root = super.getRoot();
 		Stage stage = new Stage();
 		stage.setScene(new Scene(root, 494, 414));
-		stage.getScene().getStylesheets().add(App.class.getResource("style.css").toExternalForm());
+		stage.getScene().getStylesheets().add(App.class.getResource("css/style.css").toExternalForm());
 		stage.show();
 
 		// TODO: relative path
-		emoji = new Emoji("I:\\Win10\\Dropbox\\Studium\\SoSe2016\\Projekt\\SharkNetJavaFX\\sharknet-javafx\\src\\main\\resources\\net\\sharksystem\\sharknet\\javafx\\images\\emojione-sprites.png");
+
+		try {
+			emoji = new Emoji(App.class.getResource("images/emojione-sprites.png").openStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		emoji.loadEmojis();
 		emojis = emoji.getEmojis();
 
 		for (int i = 0; i < Emoji.cols; i++) {
 			for (int j = 0; j < Emoji.rows; j++) {
 				ImageView smiley = new ImageView();
-				smiley.setFitHeight(64);
-				smiley.setFitWidth(64);
+				smiley.setFitHeight(32);
+				smiley.setFitWidth(32);
+				smiley.setPreserveRatio(true);
 
 				ByteArrayOutputStream os = new ByteArrayOutputStream();
 				try {
+
 					ImageIO.write(emojis[i][j], "png", os);
 					InputStream is = new ByteArrayInputStream(os.toByteArray());
 					smiley.setImage(new Image(is));
