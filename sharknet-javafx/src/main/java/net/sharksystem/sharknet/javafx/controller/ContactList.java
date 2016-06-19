f8f9150139eed961fc9532178ae336504ec96948
Johann <johann.w@live.de>
2016-06-19 19:49:09 +0200

    kleine änderungen Settings, Sync, Kontaktliste

---
+++ sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/ContactList.java
@@ -0,0 +1,16 @@
+package net.sharksystem.sharknet.javafx.controller;
+
+
+import net.sharksystem.sharknet.api.Contact;
+import net.sharksystem.sharknet.api.Feed;
+import net.sharksystem.sharknet.javafx.controller.inbox.InboxEntryController;
+import net.sharksystem.sharknet.javafx.controls.medialist.MediaListCell;
+import net.sharksystem.sharknet.javafx.controls.medialist.MediaListView;
+
+public class ContactList extends MediaListView<Contact> {
+
+	public ContactList() {
+		super();
+		setCellFactory(param -> new MediaListCell<Contact>(ContactListEntryController.class));
+	}
+}
