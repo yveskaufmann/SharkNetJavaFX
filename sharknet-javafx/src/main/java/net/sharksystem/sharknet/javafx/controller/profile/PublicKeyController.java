49da8b8787cc70e30319a3846b5b91dc79176815
Yves Kaufmann <fxdapokalypse@googlemail.com>
2016-07-09 03:46:10 +0200

    add renewKeys invoke

--- sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/profile/PublicKeyController.java
+++ sharknet-javafx/src/main/java/net/sharksystem/sharknet/javafx/controller/profile/PublicKeyController.java
@@ -2,7 +2,9 @@ package net.sharksystem.sharknet.javafx.controller.profile;
 
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import net.sharksystem.sharknet.api.Contact;
 import net.sharksystem.sharknet.api.Profile;
@@ -11,6 +13,8 @@ import net.sharksystem.sharknet.javafx.i18n.I18N;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.Date;
+
+import static net.sharksystem.sharknet.javafx.i18n.I18N.getString;
 
 /**
  * @Author Yves Kaufmann
@@ -53,12 +57,12 @@ public class PublicKeyController {
 		if (keyExpiration == null) keyExpiration = new Date();
 
 		publicKeyField.setText(key);
-		expirationField.setText(I18N.getString("profile.publicKey.expiration", keyExpiration));
+		expirationField.setText(getString("profile.publicKey.expiration", keyExpiration));
 
-		generateNewPublicKey.setOnAction((e) -> {
-			throw new NotImplementedException("Not yet implemented: public key generation");
-		});
+		generateNewPublicKey.setOnAction((e) -> onGenerateNewKeyClicked(myProfile));
 	}
+
+
 
 	/******************************************************************************
 	 *
@@ -66,4 +70,16 @@ public class PublicKeyController {
 	 *
 	 ******************************************************************************/
 
+	private void onGenerateNewKeyClicked(Profile myProfile) {
+		Alert shouldPublicKeyGenerated = new Alert(Alert.AlertType.INFORMATION);
+		shouldPublicKeyGenerated.setContentText(getString("%profile.publicKey.generate.dlg.confirmation"));
+		shouldPublicKeyGenerated.getDialogPane().getStyleClass().add("theme-preset");
+		shouldPublicKeyGenerated.initOwner(generateNewPublicKey.getScene().getWindow());
+		shouldPublicKeyGenerated.showAndWait().ifPresent((buttonType) -> {
+			if (ButtonType.OK.equals(buttonType)) {
+				// TODO: add a progress bar but first the api must provide a specific interface
+				myProfile.renewKeys();
+			}
+		});
+	}
 }
